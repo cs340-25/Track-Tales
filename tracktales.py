fc0319a24e04ead513e059d4190a05559e309655
shipra0803 <spate201@vols.utk.edu>
2025-03-06 09:34:01 -0600

    Update tracktales.py

--- tracktales.py
+++ tracktales.py
@@ -3,3 +3,8 @@ sp = Spotify(auth_manager = SpotifyOAuth(client_id = "CLIENT_ID",
                                          client_secret = "CLIENT_SECRET", 
                                          redirect_uri = "http://localhost:8888/callback", scope="user-library-read"))
 results = sp.current_user_saved_tracks()
+
+# MongoDB setup
+client = pymongo.MongoClient("mongodb://localhost:27017/")
+db = client["spotify_data"]
+collection = db["liked_songs"]
