877e5d740b5203fb0d656ba8f664dcdc8797dbb2
lenayoung8 <153099057+lenayoung8@users.noreply.github.com>
2025-03-12 13:08:21 -0400

    Finished sprint 3

--- tracktales.py
+++ tracktales.py
@@ -20,3 +20,25 @@ if __name__ == "__main__":
     collection.insert_many(songs_list)
 
     app.run(debug=True)
+
+# Function processes Json responses from API call
+def fetch_liked_songs():
+    results = sp.current_user_saved_tracks()
+    songs_list = []
+
+    # Go through responses & get various data from each song
+    for item in results["items"]:
+        track = item["track"]
+        song_data = {
+            "name": track["name"],
+            "artist": track["artists"][0]["name"],
+            "artist_id": track["artists"][0]["id"],  # Needed for genre lookup
+            "album": track["album"]["name"],
+            "genre": "Unknown",
+            "value": 1,
+            "parent": track["artists"][0]["name"]
+        }
+        # Add the song's data to a song list array
+        songs_list.append(song_data)
+
+    return songs_list
