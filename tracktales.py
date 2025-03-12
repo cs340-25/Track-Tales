0e8a771d8475f6046647d665b2e667b8be9b637a
Mia Patrikios <118705616+mpatrikios@users.noreply.github.com>
2025-03-12 12:19:03 -0400

    Update tracktales.py
    Added imports and call to insert songs into MongoDB

--- tracktales.py
+++ tracktales.py
@@ -1,3 +1,8 @@
+import pymongo
+from flask import Flask, render_template
+from spotipy import Spotify
+from spotipy.oauth2 import SpotifyOAuth
+
 # Setup Spotify API to access user's songs
 sp = Spotify(auth_manager = SpotifyOAuth(client_id = "CLIENT_ID", 
                                          client_secret = "CLIENT_SECRET", 
@@ -8,3 +13,10 @@ results = sp.current_user_saved_tracks()
 client = pymongo.MongoClient("mongodb://localhost:27017/")
 db = client["spotify_data"]
 collection = db["liked_songs"]
+
+if __name__ == "__main__":
+    # Clear old data and insert new data
+    collection.delete_many({})
+    collection.insert_many(songs_list)
+
+    app.run(debug=True)
